1a3a0384dadaab6cdd175495e89c00cda1cf88d8
Luis <villazonpersonal@gmail.com>
2016-07-05 21:28:54 +0200

    Clean scout code

--- src/scout.ts
+++ src/scout.ts
@@ -1,6 +1,6 @@
 /// <reference path="./typings/tsd.d.ts"/>
 
-import events = require('events');
+import events = require("events");
 
 import * as Message from "./message";
 import {Type as Type} from "./network/network";
@@ -9,18 +9,18 @@ import {Type as Type} from "./network/network";
  * Class Scout
  * @class Scout
  */
-export class Scout extends events.EventEmitter{
+export class Scout extends events.EventEmitter {
 
   private acceptors: any;
-  private acceptorsResponsed : Array<string>
-  private pvalues : Array<any>
-  private slotsOfValues : Array<any>
-  private adopted:boolean = false;
-  private ballot:Ballot
-  private network:any
+  private acceptorsResponsed: Array<string>;
+  private pvalues: Array<any>;
+  private slotsOfValues: Array<any>;
+  private adopted: boolean = false;
+  private ballot: Ballot;
+  private network: any;
 
 
-  constructor(params:{ballot:Ballot; network?:any}){
+  constructor(params: {ballot: Ballot, network?: any}) {
     super();
     this.slotsOfValues = [];
     this.pvalues = [];
@@ -29,78 +29,83 @@ export class Scout extends events.EventEmitter{
     this.adopted = false;
     this.ballot = params.ballot;
     this.network = params.network;
-    this.network.on('P1B', (message) => { this.process(message) });
+    this.network.on("P1B", (message) => {
+       this.process(message);
+     });
   }
 
-  start( ){
-      var operation = {
+  start( ) {
+      let operation = {
           leader: this.ballot.id,
           ballot: this.ballot,
       };
-    var message = new Message.Message({ type: 'P1A', from: this.ballot.id, command_id: 0, operation: operation });
+    let message = new Message.Message({ type: "P1A",
+                                        from: this.ballot.id,
+                                        command_id: 0,
+                                        operation: operation });
     this.network.send(message, Type.PUB);
     return message;
   }
 
 
-  private process(message: any) {
+  private process(message: Message.Message) {
     if (this.adopted) return;
-    if (message.type === 'P1B') var ballot = message.operation.ballot;
-    if(this.ballot.isEqual(ballot)){
+    const ballot = message.operation.ballot;
+    if (this.ballot.isEqual(ballot)) {
         this.updateAcceptedValuesAndAcceptorsResponse(message);
         this.ifResponseMayorOfAcceptorsSendAdopted();
-    }else if(!this.ballot.isEqual(ballot) || this.adopted){
+    }else if (!this.ballot.isEqual(ballot) || this.adopted) {
         this.sendPreemptedMessage(ballot);
     }
   }
 
-  private updateAcceptedValuesAndAcceptorsResponse(message:any){
-    this.addAcceptedToPValues(message.operation.accepted)
+  private updateAcceptedValuesAndAcceptorsResponse(message: Message.Message) {
+    this.addAcceptedToPValues(message.operation.accepted);
     if (this.acceptorsResponsed.indexOf(message.from) < 0 && this.acceptors.has(message.from))
-      this.acceptorsResponsed.push(message.from)
+      this.acceptorsResponsed.push(message.from);
   }
 
-  private ifResponseMayorOfAcceptorsSendAdopted(){
+  private ifResponseMayorOfAcceptorsSendAdopted() {
 
-    var numberAcceptors = this.acceptors.size;
+    let numberAcceptors = this.acceptors.size;
     if (numberAcceptors === 2)
-      numberAcceptors = 3
-    if (this.acceptorsResponsed.length >= Math.round( numberAcceptors / 2 )){
-      this.adopted = true
-      this.acceptorsResponsed = []
+      numberAcceptors = 3;
+    if (this.acceptorsResponsed.length >= Math.round( numberAcceptors / 2 )) {
+      this.adopted = true;
+      this.acceptorsResponsed = [];
 
-      var pvaluesMap = new Map()
-      var pvaluesInSlotMap = new Map()
+      let pvaluesMap = new Map();
+      let pvaluesInSlotMap = new Map();
 
-      for(var pvalue of this.pvalues){
-        pvaluesMap.set(pvalue.slot , pvalue)
-        pvaluesInSlotMap.set(pvalue , pvalue.slot)
+      for (let pvalue of this.pvalues){
+        pvaluesMap.set(pvalue.slot , pvalue);
+        pvaluesInSlotMap.set(pvalue , pvalue.slot);
       }
-      var body = {
+      let body = {
         ballot: this.ballot,
         pvalues: pvaluesMap,
         pvaluesSlot: pvaluesInSlotMap
-      }
-      this.emit('adopted', body);
-      this.network.removeAllListeners('message');
+      };
+      this.emit("adopted", body);
+      this.network.removeAllListeners("message");
     }
   }
 
-  private sendPreemptedMessage(ballot:Ballot){
-      var body = {
+  private sendPreemptedMessage(ballot: Ballot) {
+      let body = {
           ballot: ballot,
           pvalues: this.pvalues
       };
     this.adopted = true;
-    this.emit('preempted', body);
-    this.network.removeAllListeners('message');
+    this.emit("preempted", body);
+    this.network.removeAllListeners("message");
   }
 
-  private addAcceptedToPValues(accepted:any){
-    for(var accept of accepted){
-      if(this.slotsOfValues.indexOf(accept.slot) < 0){
-        this.pvalues.push(accept)
-        this.slotsOfValues.push(accept.slot)
+  private addAcceptedToPValues(accepted: any) {
+    for (let accept of accepted){
+      if (this.slotsOfValues.indexOf(accept.slot) < 0) {
+        this.pvalues.push(accept);
+        this.slotsOfValues.push(accept.slot);
       }
     }
   }
